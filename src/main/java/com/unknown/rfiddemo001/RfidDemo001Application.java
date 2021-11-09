64b8b7caaae0282b188bdfafd8cf7c111270a7d3
dipee <dipen.2052@gmail.com>
2021-11-09 15:36:59 +0545

    simple websocekt chat implemented

--- src/main/java/com/unknown/rfiddemo001/RfidDemo001Application.java
+++ src/main/java/com/unknown/rfiddemo001/RfidDemo001Application.java
@@ -2,12 +2,26 @@ package com.unknown.rfiddemo001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RfidDemo001Application {
 
     public static void main(String[] args) {
         SpringApplication.run(RfidDemo001Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 
 }
